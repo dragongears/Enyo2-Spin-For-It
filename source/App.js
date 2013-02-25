417558d6dbf5636883f9887b708545aa556e276e
Art Dahm <art@dragongears.com>
2013-02-24 22:32:26 -0800

    More attempts to smooth animation on Android.

--- source/App.js
+++ source/App.js
@@ -4,20 +4,21 @@ enyo.kind({
 	handlers: {'ontap': 'spin'},
 	style: "width:100%",
 	components: [
-		{name: 'pointer', style: "-webkit-transform:translateZ(0);-o-transform:translateZ(0);-moz-transform:translateZ(0);transform:translateZ(0);", kind: enyo.Image, src: "assets/hand_1024.png"},
-		{name: "animator", kind: enyo.Animator, duration: 4000, onStep: "stepAnimation"}
+		{name: 'pointer', style: "-webkit-transform:translateZ(0);-o-transform:translateZ(0);-moz-transform:translateZ(0);transform:translateZ(0);", kind: enyo.Image, src: "assets/hand_512.png"},
+		{name: "animator", kind: enyo.Animator, duration: 5000, onStep: "stepAnimation"}
 	],
 
 	stepAnimation: function(inSender, inValue) {
-		enyo.dom.transform(this.$.pointer, {rotate: (inSender.value) + 'deg'});
+		enyo.dom.transform(this.$.pointer, {rotate: (inSender.value % 360) + 'deg'});
+//		this.$.pointer.setStyle('-webkit-transform: rotate('+ (inSender.value % 360) +'deg)');
 	},
 
 	spin: function() {
-		this.$.animator.play({startValue: 0, endValue: (360*4) + (Math.floor(Math.random() * 360)), easingFunction: enyo.easing.cubicOut});
+		this.$.animator.play({startValue: 0, endValue: (3600) + (Math.floor(Math.random() * 360)), easingFunction: enyo.easing.cubicOut});
 	},
 
 	resizePointer: function() {
-		var min = Math.min(this.hasNode().clientWidth *.70, this.hasNode().clientHeight *.70, 1024);
+		var min = Math.min(this.hasNode().clientWidth *.70, this.hasNode().clientHeight *.70, 512);
 		this.$.pointer.applyStyle("width", min+"px");
 		this.$.pointer.applyStyle("margin-top", this.hasNode().clientHeight/2-min/2+"px");
 	},
@@ -25,6 +26,10 @@ enyo.kind({
 	rendered: function() {
 		this.inherited(arguments);
 		this.resizePointer();
+		var self = this;
+		setTimeout(function() {
+			self.spin();
+		}, 2000);
 	},
 
 	resizeHandler: function(){
