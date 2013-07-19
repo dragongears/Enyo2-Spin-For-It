1eefc5b96aa23b24b085069238045fcc15fd994d
art.dahm <art.dahm@lge.com>
2013-07-19 16:36:30 -0700

    add grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+	  pkg: grunt.file.readJSON('package.json'),
+		clean: ["deploy/*", "build/*"],
+		compress: {
+		  "phonegap-build": {
+		    options: {
+		      archive: 'deploy/<%= pkg.id %>_<%= pkg.version %>.zip'
+		    },
+		    files: [
+		      {expand: true, cwd: 'deploy/<%= pkg.name %>/', src: ['**'], dest: ''},
+		      {expand: true, src: ['config.xml'], dest: ''}
+		    ]
+		  }
+		},
+		"phonegap-build": {
+			debug: {
+				options: {
+					archive: "deploy/<%= pkg.id %>_<%= pkg.version %>.zip",
+					"appId": "367891",
+					"user": {
+						"email": "art@dragongears.com",
+						"password": "binky1802"
+					}
+				}
+			},
+			release: {
+				options: {
+					"isRepository": "true",
+					"appId": "367891",
+					"user": {
+						"token": "zj3PNEbTNLpzCxDk8hHB"
+					}
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-phonegap-build');
+
+	grunt.registerTask('default', ['clean', 'deploy', 'compress', 'palm-package']);
+
+	grunt.registerTask('deploy', 'Create a deployable Enyo app.', function() {
+		var shell = require('shelljs');
+
+		grunt.log.writeln('Deploying app');
+		shell.exec('sh tools/deploy.sh');
+
+		grunt.log.writeln('Copying PhoneGap Build config.xml file...');
+		grunt.file.copy('config.xml', 'deploy/' + grunt.config('pkg.name') + '/config.xml');
+
+		grunt.log.writeln('Creating webOS appinfo.json file from package.json...');
+		grunt.file.copy('package.json', 'deploy/' + grunt.config('pkg.name') + '/appinfo.json');
+	});
+
+	grunt.registerTask('palm-package', 'Package a webOS *.ipk from deployed app.', function() {
+		var shell = require('shelljs');
+
+		grunt.log.writeln('Packaging webOS app...');
+		shell.exec('palm-package -o deploy deploy/' + grunt.config('pkg.name') + '/');
+	});
+
+};
